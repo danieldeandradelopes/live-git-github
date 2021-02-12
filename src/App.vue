9e31ee8c3d5e741f1eaf06a9282fbbed80c24159
danieldeandradelopes <danieldeandradelopes@gmail.com>
2021-02-12 20:30:36 -0300

    segundo commit

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <template>
   <div id="q-app">
+    <!-- MEU PRIMEIRO COMMIT -->
     <router-view />
   </div>
 </template>
 <script>
 export default {
-  name: 'App'
-}
+  name: "App"
+};
 </script>
