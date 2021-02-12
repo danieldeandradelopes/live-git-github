975b2f1ed5776d6081a22e52e7078c2872dab344
danieldeandradelopes <danieldeandradelopes@gmail.com>
2021-02-12 20:39:21 -0300

    change file Index.vue, add new h1

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,14 +1,12 @@
 <template>
   <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-full.svg"
-    >
+    <h1>Teste de merge</h1>
+    <img alt="Quasar logo" src="~assets/quasar-logo-full.svg" />
   </q-page>
 </template>
 
 <script>
 export default {
-  name: 'PageIndex'
-}
+  name: "PageIndex"
+};
 </script>
